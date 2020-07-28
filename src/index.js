15c0074ad03ea6bf7015efbc10138d7f722e9c0b
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 16:15:47 -0500

    finish getting Library back to working order

--- src/index.js
+++ src/index.js
@@ -89,34 +89,7 @@ class Library extends Component {
   }
 }
 
-class FavoriteColorForm extends Component {
-  state = {
-    value: ''
-  }
-
-  newColor = e =>
-    this.setState({ value: e.target.value })
-
-  submit = e => {
-    console.log(`New Color: ${this.state.value}`)
-    e.preventDefault()
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.submit}>
-        <label>Favorite Color:
-          <input
-            type="color"
-            onChange={this.newColor} />
-        </label>
-        <button>Submit</button>
-      </form>
-    )
-  }
-}
-
 render(
-  <FavoriteColorForm />,
+  <Library books={bookList} />,
   document.getElementById('root')
 )
