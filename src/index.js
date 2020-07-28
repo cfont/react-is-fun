35f54e3666df74f5fdb0fd9a450d156a17b012cd
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 16:59:08 -0500

    moved Book component

--- src/index.js
+++ src/index.js
@@ -9,16 +9,7 @@ let bookList = [
   { title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 304 },
 ];
 
-const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
-  return (
-    <section>
-      <h2>{title}</h2>
-      <p>by: {author}</p>
-      <p>Pages: {pages}</p>
-      <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!'}</p>
-    </section>
-  )
-}
+
 
 const Hiring = () =>
   <div>
