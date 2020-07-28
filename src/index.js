6b65ac66b7153e87c7787037e77730b08b3184cb
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 16:52:53 -0500

    create error because bookList set to number

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import PropTypes from 'prop-types'
 
-let bookList = [
-  { title: "The Sun Also Rises", author: "Ernest Hemingway", pages: 260 },
-  { title: "Hunger", author: "Roxane Gay", pages: 320 },
-  { title: "White Teeth", author: "Zadie Smith", pages: 480 },
-  { title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 304 },
-];
+let bookList = 0;
 
 const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
   return (
