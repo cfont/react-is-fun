a260bc960c6bee4845d325724aa69d715bf2c911
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 14:55:27 -0500

    add two components to be conditionally displayed
    based on state

--- src/index.js
+++ src/index.js
@@ -19,10 +19,22 @@ const Book = ({title, author, pages, freeBookmark}) => {
   )
 }
 
+const Hiring = () =>
+  <div>
+    <p>The library is hiring. Go to www.library.com/jobs for more.</p>
+  </div>
+
+const NotHiring = () =>
+  <div>
+    <p>The library is not hiring. Check back later for more info.</p>
+  </div>
+
+
 class Library extends Component {
   state = {
     open: true,
-    freeBookmark: true
+    freeBookmark: true,
+    hiring: false
   }
 
   toggleOpenClosed = () => {
@@ -32,10 +44,10 @@ class Library extends Component {
   }
 
   render() {
-    console.log(this.state)
     const {books} = this.props
     return (
       <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
         <h1>The library is {this.state.open ? "open" : "closed"}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map((book, i) => (
