4bd7105c392c7f4423177302f372166f51a37531
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 10:59:03 -0500

    starting to work on multiple components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-}
-
-const getPercent = decimal => {
-  return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total / goal)
-}
-
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+const Library = () => {
   return (
-    <section>
-      <div>
-        <p>Total Days: {total}</p>
-      </div>
-      <div>
-        <p>Powder Days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountr Days: {backcountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calcGoalProgress(total,goal)}</p>
-      </div>
-    </section>
+    <div>
+      Welcome to the Library
+    </div>
   )
 }
 
 render(
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}
-  />,
+  <Library />,
   document.getElementById('root')
 )
