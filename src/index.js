ffbb996c451a8165a58e16c5fab2205140edbd62
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 13:38:32 -0500

    add button to toggle open/closed

--- src/index.js
+++ src/index.js
@@ -38,15 +38,16 @@ class Library extends Component {
     const {books} = this.props
     return (
       <div>
-        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
-        {books.map(
-          (book, i) =>
-            <Book
-              key={i}
-              title={book.title}
-              author={book.author}
-              pages={book.pages} />
-        )}
+        <h1>The library is {this.state.open ? "open" : "closed"}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map((book, i) => (
+          <Book
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+          />
+        ))}
       </div>
     );
   }
