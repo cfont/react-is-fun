e56016e557ccf56f413c3a6ba6e02986601c65ca
Chris Fontenot <chris.fontenot@gmail.com>
2020-07-28 11:01:02 -0500

    added Book component after Library component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+const Book = ({title, author, pages}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+      <p>by: {author}</p>
+      <p>Pages: {pages}</p>
+    </section>
+  )
+}
+
 const Library = () => {
   return (
     <div>
